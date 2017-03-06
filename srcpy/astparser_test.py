5511d4382d5983c2f0d98ab9bec599dee61e4fcc
thdiaman <themisdiamantopoulos@hotmail.com>
2017-03-06 16:51:32 +0200

    Fix version in test for python binding

--- srcpy/astparser_test.py
+++ srcpy/astparser_test.py
@@ -2,7 +2,7 @@ from astparser import ASTParser
 
 if __name__ == '__main__':
 	'''Used as a test for the python bindings'''
-	ast_parser = ASTParser("../target/agora-ast-parser-0.1-0.4.jar")
+	ast_parser = ASTParser("../target/agora-ast-parser-0.1.jar")
 	ast = ast_parser.parse_string(
 			"import org.myclassimports;\n" + 
 			"\n" + 
